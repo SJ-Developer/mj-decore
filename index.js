570b4334f80596ff329b9b68984c7073a2c2991a
SJ-Developer <84143081+SJ-Developer@users.noreply.github.com>
2021-08-05 13:07:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function show()
-{
-    var name = document.getElementById("name").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var textarea = document.getElementById("textarea").value;
-//   console.log("Name: "+name+"\nPhone no.: "+phone+"\nEmail-Id: "+email+"\nPassword: "+password+"\nSuggestions: "+textarea);
-  var visitor = { Name:name, Phone:phone, Email:email, Password:password, Suggestions:textarea };
-  // console.log(visitor);
-  confirm("Name: " + name + " Phone: " + phone + " Email-ID: " + email + " Password: " + password + " Suggestions: " + textarea);
-}
+/**
+ * Merge object b with object a.
+ *
+ *     var a = { foo: 'bar' }
+ *       , b = { bar: 'baz' };
+ *
+ *     merge(a, b);
+ *     // => { foo: 'bar', bar: 'baz' }
+ *
+ * @param {Object} a
+ * @param {Object} b
+ * @return {Object}
+ * @api public
+ */
 
-function reset(){  
-  // confirm("Reset?");
-  document.getElementById("name").value = "";  
-  document.getElementById("phone").value = "";  
-  document.getElementById("email").value = "";  
-  document.getElementById("password").value = "";  
-  document.getElementById("textarea").value = "";  
-} 
-
+exports = module.exports = function(a, b){
+  if (a && b) {
+    for (var key in b) {
+      a[key] = b[key];
+    }
+  }
+  return a;
+};
