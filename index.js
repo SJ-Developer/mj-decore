e7ab4397fc3de95ae0d84991c48dc868ccda9b26
SJ-Developer <84143081+SJ-Developer@users.noreply.github.com>
2021-08-06 12:26:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function show()
+{
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var textarea = document.getElementById("textarea").value;
+//   console.log("Name: "+name+"\nPhone no.: "+phone+"\nEmail-Id: "+email+"\nPassword: "+password+"\nSuggestions: "+textarea);
+  var visitor = { Name:name, Phone:phone, Email:email, Password:password, Suggestions:textarea };
+  // console.log(visitor);
+  confirm("Name: " + name + " Phone: " + phone + " Email-ID: " + email + " Password: " + password + " Suggestions: " + textarea);
+}
+
+function reset(){  
+  // confirm("Reset?");
+  document.getElementById("name").value = "";  
+  document.getElementById("phone").value = "";  
+  document.getElementById("email").value = "";  
+  document.getElementById("password").value = "";  
+  document.getElementById("textarea").value = "";  
+} 
+
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightNav = document.querySelector('.rightNav')
+navList = document.querySelector('.nav-list')
+
+burger.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+})
